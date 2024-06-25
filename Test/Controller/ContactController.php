42721f43766e1c59370a71555a0755d2ffd08998
Ilisei Daria-Stefania <iliseidaria27@gmail.com>
2024-06-25 03:07:20 +0300

    update folders

--- Test/Controller/ContactController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class ContactController {
-    public function index() {
-        
-        require_once BASE_PATH . '/View/Contact.php';
-    }
-}
-?>
--- Test/Controller/DataVisualizationController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class DataVisualizationController {
-    public function index() {
-        
-        require_once BASE_PATH . '/View/visualizeData.php';
-    }
-}
-?>
